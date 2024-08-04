80985c09f05e407036afb370fe701c9768e0f218
Marynryn <murmar95@gmail.com>
2024-08-04 22:56:06 +0300

    reviews,footer, page-2, page-3 html

---
+++ src/js/faq.js
@@ -0,0 +1,6 @@
+document.querySelectorAll('.faq-toggle').forEach(button => {
+  button.addEventListener('click', () => {
+    const item = button.closest('.faq-item');
+    item.classList.toggle('open');
+  });
+});
